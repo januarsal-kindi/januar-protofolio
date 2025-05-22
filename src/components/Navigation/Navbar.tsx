53ed4ef9d1a880cc03cf22c0efc14ca7eac00fa3
januarsal-kindi <yayan.yayan98@gmail.com>
2025-05-22 17:38:53 +0700

    fix build

--- src/components/Navigation/Navbar.tsx
+++ src/components/Navigation/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import Link from "next/link";
-import BurgerIcon from "@/components/icons/burger.svg";
 import NavbarMobile from "./NavbarMobile";
 const menus = [
   {
